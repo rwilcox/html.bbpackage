16c0bc000e6440fdf5d538f5d4affa12cf6185c1
Ryan Wilcox <rwilcox@wilcoxd.com>
2012-08-23 10:07:50 -0400

    Put CSSComb.com into a BBEdit filter

---
+++ Contents/Text Filters/HTML in BBEdit/csscomb.php
@@ -0,0 +1,13 @@
+#!/usr/bin/env php
+
+<?php
+/**
+ * CSScomb 2.11 (build 3ded73c-1208080145)
+ * http://CSScomb.com
+ * http://twitter.com/CSScomb
+ */
+require( dirname(__FILE__). '/../../SharedSupport/csscomb/csscomb.php' );
+$input = file_get_contents('php://stdin');
+$c = new csscomb();
+echo $c->csscomb($input);
+?>
